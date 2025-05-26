e615ddb3452f6b5972dd5b9f05fda66d8f2e75e5
Patrícia Silva <prrs89@gmail.com>
2025-05-26 14:45:41 +0100

    [1 Inheritance] 1.1 Inheritance between classes

---
+++ src/inheritance/InheritanceChecker.java
@@ -0,0 +1,21 @@
+package inheritance;
+
+/*
+Inheritance is one of the pillars of object-oriented programming.
+It's particularly useful for reusing code between related classes.
+*/
+public class InheritanceChecker {
+
+    public static void main(String[] args) {
+        /*
+        Anything that's marked as private is not inherited.
+        Only protected and public fields and methods can be inherited.
+        */
+
+        Person person = new Person();
+        // person.
+
+        Employee employee = new Employee();
+        // employee.
+    }
+}
